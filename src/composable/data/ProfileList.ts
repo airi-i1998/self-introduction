ef3e203467079da1cc5bbb11b444644057d733b2
airi-i1998 <airi.inoue@generosity.co.jp>
2024-09-29 15:55:46 +0900

    add: profile List

---
+++ src/composable/data/ProfileList.ts
@@ -0,0 +1,14 @@
+export const profileList = [
+  {
+    img: "/personal.png",
+    title: "personal",
+  },
+  {
+    img: "/work-history.png",
+    title: "work-history",
+  },
+  {
+    img: "/skills.png",
+    title: "skills",
+  },
+]
