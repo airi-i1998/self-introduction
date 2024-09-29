081cf8c8022c5dfe70b980f15820cbacb9959d54
airi-i1998 <airi.inoue@generosity.co.jp>
2024-09-29 17:08:50 +0900

    add: personalList

---
+++ src/composable/form/personalList.ts
@@ -0,0 +1,25 @@
+export const personalList = [
+  {
+    img: "/profile-img.png"
+  },
+  {
+    label: "名前",
+    text: "テスト（仮）",
+    type: "text"
+  },
+  {
+    label: "生年月日",
+    text: "テスト（仮）",
+    type: "text"
+  },
+  {
+    label: "性別",
+    text: "テスト（仮）",
+    type: "text"
+  },
+  {
+    label: "メールアドレス",
+    text: "airi.inoue@generosity.co.jp",
+    type: "email"
+  },
+]
